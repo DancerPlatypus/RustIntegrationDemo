159706f1e5fac469e0cac4245602863429e92396
Mattia Barini <m.barini@betacom.it>
2024-04-12 17:15:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+
+
+public class Program
+{
+    [DllImport(@"C:\Users\m.barini\Desktop\Projects\cs_call_rust\target\debug\our_rust.dll")]
+    private static extern int hello_world();
+
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(hello_world());
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message); ;
+        }
+    }
+}
